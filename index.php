3c88fd4ca13741d50261a389138ec83c58f2c547
Ankit <kitilicous@gmail.com>
2013-04-13 05:28:42 -0700

    Shuffel Freinds

--- index.php
+++ index.php
@@ -60,7 +60,7 @@ if ($user_id) {
   $likes = idx($facebook->api('/me/likes?limit=10'), 'data', array());
 
   // This fetches 4 of your friends.
-  $friends = idx($facebook->api('/me/friends?limit=100'), 'data', array());
+  $friends = idx($facebook->api('/me/friends?'), 'data', array());
   shuffle($friends);
 
   // And this returns 16 of your photos.
